3d71d7083002e49e2facb961152c49eac136a72f
soekmego <soekmen.goekhan@gmail.com>
2017-05-27 20:42:04 +0200

    changed codestructure

---
+++ main.py
@@ -0,0 +1,60 @@
+#Python top down shooter made with pygame.
+#Still work in progress.
+
+import pygame as pg
+from settings import *
+from sprites import *
+
+class Game:
+    def __init__(self):
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        self.all_sprites.update()
+
+    def events(self):
+        for event in pg.event.get():
+            #check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        #After drawing everything, flip display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        pass
+
+    def show_go_screen(self):
+        pass
+
+
+g = Game()
+g.show_start_screen()
+
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pg.quit()
---
+++ settings.py
@@ -0,0 +1,13 @@
+#Settings for supershoot
+
+WIDTH = 360
+HEIGHT = 480
+FPS = 30
+TITLE = "insert game name here"
+
+#Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
