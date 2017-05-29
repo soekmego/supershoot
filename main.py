8d4e70bcf450bb8cd67912cffc1508aa04f96b51
soekmego <soekmen.goekhan@gmail.com>
2017-05-29 17:54:31 +0200

    added player characterand grid based movement

--- main.py
+++ main.py
@@ -2,46 +2,75 @@
 #Still work in progress.
 
 import pygame as pg
+import sys
 from settings import *
 from sprites import *
 
 class Game:
     def __init__(self):
         pg.init()
-        pg.mixer.init()
         self.screen = pg.display.set_mode((WIDTH, HEIGHT))
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
-        self.running = True
+        pg.key.set_repeat(500, 100)
+        self.load_data()
+
+    def load_data(self):
+        pass
 
     def new(self):
+        # initialize all variables and do all the setup for a new game
         self.all_sprites = pg.sprite.Group()
-        self.run()
+        self.walls = pg.sprite.Group()
+        self.player = Player(self, 10, 10)
+        for x in range(10, 20):
+            Wall(self, x, 5)
 
     def run(self):
+        # game loop - set self.playing = False to end the game
         self.playing = True
         while self.playing:
-            self.clock.tick(FPS)
+            self.dt = self.clock.tick(FPS) / 1000
             self.events()
             self.update()
             self.draw()
 
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
     def update(self):
+        # update portion of the game loop
         self.all_sprites.update()
 
-    def events(self):
-        for event in pg.event.get():
-            #check for closing window
-            if event.type == pg.QUIT:
-                if self.playing:
-                    self.playing = False
-                self.running = False
+    def draw_grid(self):
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
     def draw(self):
-        self.screen.fill(BLACK)
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
         self.all_sprites.draw(self.screen)
-        #After drawing everything, flip display
         pg.display.flip()
+
+    def events(self):
+        # catch all events here
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
 
     def show_start_screen(self):
         pass
@@ -49,12 +78,10 @@ class Game:
     def show_go_screen(self):
         pass
 
-
+# create the game object
 g = Game()
 g.show_start_screen()
-
-while g.running:
+while True:
     g.new()
-    g.show_go_screen()
-
-pg.quit()
+    g.run()
+g.show_go_screen()
--- settings.py
+++ settings.py
@@ -1,13 +1,20 @@
-#Settings for supershoot
-
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-TITLE = "insert game name here"
-
-#Define colors
+#Define colors in RGB values
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+#Screen and display settings
+WIDTH = 1024 # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768 # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60
+TITLE = "supershoot"
+BGCOLOR = DARKGREY
+
+TILESIZE = 32
+GRIDWIDTH = WIDTH / TILESIZE
+GRIDHEIGHT = HEIGHT / TILESIZE
